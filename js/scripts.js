6acb86ff18fa04c9e32e1fd339091ab2ec10f96c
Alex Guizar <alexguizar90@gmail.com>
2021-06-23 16:17:19 -0700

    Replaced test JS with array of pokemon info

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,39 @@
-alert('Hello World!');
+const pokemonList = [];
 
-const favoriteFood = 'Ramen!';
+pokemonList.push({
+  name: 'Charmander',
+  height: 0.7,
+  types: ['fire']
+});
 
-document.write(favoriteFood);
+pokemonList.push({
+  name: 'Gengar',
+  height: 1.5,
+  types: ['ghost', 'poison']
+});
+
+pokemonList.push({
+  name: 'Gyarados',
+  height: 6.5,
+  types: ['water', 'flying']
+});
+
+pokemonList.push({
+  name: 'Zapdos',
+  height: 1.6,
+  types: ['electric', 'flying']
+});
+
+pokemonList.push({
+  name: 'Suicune',
+  height: 2,
+  types: ['water']
+});
+
+pokemonList.push({
+  name: 'Ho-oh',
+  height: 3.8,
+  types: ['fire', 'flying']
+});
+
+console.log(pokemonList);
