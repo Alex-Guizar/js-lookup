805aeff9d2855e35e415013aefd1ae52fd1ded5a
Alex Guizar <alexguizar90@gmail.com>
2021-06-25 18:30:05 -0700

    Changed the JS so the pokemon info is being added at the variable declaration

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,34 @@
-const pokemonList = [];
-
-pokemonList.push({
-  name: 'Charmander',
-  height: 0.7,
-  types: ['fire']
-});
-
-pokemonList.push({
-  name: 'Gengar',
-  height: 1.5,
-  types: ['ghost', 'poison']
-});
-
-pokemonList.push({
-  name: 'Gyarados',
-  height: 6.5,
-  types: ['water', 'flying']
-});
-
-pokemonList.push({
-  name: 'Zapdos',
-  height: 1.6,
-  types: ['electric', 'flying']
-});
-
-pokemonList.push({
-  name: 'Suicune',
-  height: 2,
-  types: ['water']
-});
-
-pokemonList.push({
-  name: 'Ho-oh',
-  height: 3.8,
-  types: ['fire', 'flying']
-});
+let pokemonList = [
+  {
+    name: 'Charmander',
+    height: 0.7,
+    types: ['fire']
+  },
+  {
+    name: 'Gengar',
+    height: 1.5,
+    types: ['ghost', 'poison']
+  },
+  {
+    name: 'Gyarados',
+    height: 6.5,
+    types: ['water', 'flying']
+  },
+  {
+    name: 'Zapdos',
+    height: 1.6,
+    types: ['electric', 'flying']
+  },
+  {
+    name: 'Suicune',
+    height: 2,
+    types: ['water']
+  },
+  {
+    name: 'Ho-oh',
+    height: 3.8,
+    types: ['fire', 'flying']
+  }
+];
 
 console.log(pokemonList);
